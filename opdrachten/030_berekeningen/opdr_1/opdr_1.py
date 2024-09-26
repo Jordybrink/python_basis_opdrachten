7de4565353eb04a820a1d82ac8792930eac93e94
Jordybrink <97104602@st.deltion.nl>
2024-09-26 15:25:50 +0200

    030-2 done

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,22 @@
-# Opdracht 1 berekeningen
-# Naam student:
-# Groep:
+''' student info 
+Opdracht 3 tekstfuncties
+Naam student: Jordy Brink
+Groep:4ITX7
+'''
+friend1 = Adam = 23
+friend2 = Bert = 15
+friend3 = Charlie = 22
 
-# Hier komt je code...
+sum_age = Adam+Bert+Charlie
+avg_age = sum_age/3
+
+print("Dit weten we over: Adam, Bert en Charlie")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", sum_age)
+print("De gemiddelde leeftijd van deze boys is: ", avg_age)
+
+
+''' Kladblok
+joe = 23
+bob = 15
+fred = 22
+'''
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,24 @@
-# Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+""" student info 
+Opdracht 3 tekstfuncties
+Naam student: Jordy Brink
+Groep:4ITX7
+"""
 
-# Hier komt je code...
-
+""" kladblok
 c = ...
 f = ...
 
 print()
+"""
+
+input_temperatuur = input("Voer een temperatuur in (zoals: 10c of 20f): ")
+temperatuur = float(input_temperatuur[:-1])
+unit = input_temperatuur[-1].lower()
+if unit == "c":
+    fahrenheit = (temperatuur * 9 / 5) + 32
+    print(f"{temperatuur} graden Celsius is gelijk aan {fahrenheit} graden Fahrenheit.")
+elif unit == "f":
+    celsius = (temperatuur - 32) * 5 / 9
+    print(f"{temperatuur} graden Fahrenheit is gelijk aan {celsius} graden Celsius.")
+else:
+    print("Ongeldige invoer. Gebruik 'c' voor Celsius of 'f' voor Fahrenheit.")
